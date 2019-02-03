e33c0eaca5ecf2f826dd848709e1fef3c0823052
Steve Harris <doc@steveharris.me>
2019-02-03 15:46:37 +0000

    commented out run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker build -t hellouclh -f config/helloUCLH.Dockerfile .
-docker run --privileged -ti -v ${PWD}:/usr/local/bin/helloUCLH -p 8889:8888 hellouclh
+# docker build -t hellouclh -f config/helloUCLH.Dockerfile .
+# docker run --privileged -ti -v ${PWD}:/usr/local/bin/helloUCLH -p 8889:8888 hellouclh
